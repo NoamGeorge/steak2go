fca116a30ba0d5dba34a63fef8ed6a9372338caa
נ ג <noam57654@gmail.com>
2024-03-12 19:17:24 +0200

    added file

---
+++ main.py
@@ -0,0 +1,4 @@
+def print_funny_story():
+    print("Once upon a time me and my friends were planning a trivia night but we didn't settle on a date so only i made a kahoot about myself")
+
+print_funny_story()
